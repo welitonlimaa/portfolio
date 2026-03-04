d7a8fdf854d07e295b2c3029c61cdd5daad7de09
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2026-03-04 14:36:32 -0300

    refactor: redireciona portfolio

--- src/app.js
+++ src/app.js
@@ -6,24 +6,26 @@ import './styles/components/About.css';
 import './styles/components/Projects.css';
 import './styles/components/Contact.css';
 import './styles/components/Footer.css';
-import About from './components/About';
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+// import About from './components/About';
+// import Banner from './components/Banner';
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Banner />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </>
-  );
+  window.location.href = "https://welitonlimaportfolio.vercel.app/";
+  return null;
+  // return (
+  //   <>
+  //     <Header />
+  //     <Banner />
+  //     <About />
+  //     <Projects />
+  //     <Contact />
+  //     <Footer />
+  //   </>
+  // );
 }
 
 export default App;
